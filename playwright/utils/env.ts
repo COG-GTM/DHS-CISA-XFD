f93d65c89b28889cc958469c15b89e47c2bb04ab
JCantu248 <jcantu248@gmail.com>
2025-04-29 12:36:29 -0500

    Adding utils/env.ts

---
+++ playwright/utils/env.ts
@@ -0,0 +1,28 @@
+// utils/env.ts
+export function determineUrl(): string {
+  const branch = process.env.GIT_BRANCH || '';
+  const insideDocker = process.env.PW_DOCKER === 'true';
+  const insideECS = process.env.PW_ECS;
+
+  console.log(
+    `Inside Docker: ${insideDocker}, Inside ECS: ${insideECS}, Branch: ${branch}`
+  );
+  if (insideECS) {
+    if (branch === 'integration') {
+      return 'https://integration.crossfeed.cyber.dhs.gov';
+    }
+    return 'https://staging-cd.crossfeed.cyber.dhs.gov';
+  }
+
+  if (insideDocker) {
+    return 'http://xfd-frontend-1:3000';
+  }
+
+  return 'http://localhost';
+}
+
+export function determineHeadless(): boolean {
+  const insideDocker = process.env.PW_DOCKER === 'true';
+  const insideECS = process.env.PW_ECS === 'true' || !!process.env.AWS_REGION;
+  return insideDocker || insideECS;
+}
