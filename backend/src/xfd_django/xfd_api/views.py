2a17ebc3d884695975c12b8597916b19ed3826fa
DJensen94 <79864006+DJensen94@users.noreply.github.com>
2025-05-07 08:28:50 -0600

    update api docstring
    update api docstring

--- backend/src/xfd_django/xfd_api/views.py
+++ backend/src/xfd_django/xfd_api/views.py
@@ -1655,7 +1655,33 @@ async def cred_sync(
     cred_sync_data: SyncRequest,
     current_user: User = Depends(get_current_active_user),
 ):
-    """Return Credential Breach findings for a requested org with checksum verification.."""
+    """
+    Return Credential Breach findings for a provided organization.
+
+    This endpoint retrieves credential breach findings from the DMZ
+    based on the input parameters provided. The response is serialized and includes a
+    SHA-256 checksum in the headers for integrity verification.
+
+    ### Request Body Parameters (SyncRequest):
+    - **page** (int, default=1):
+    Page number for pagination of the results.
+
+    - **page_size** (int, optional, default=25):
+    Number of records per page.
+
+    - **acronym** (str):
+    Organization acronym to filter the results.
+
+    - **since_date** (datetime):
+    Return results updated or found since this date.
+
+    ### Headers:
+    - **X-Salted-Checksum**:
+    A SHA-256 hash of the salted response body for response integrity verification.
+
+    ### Returns:
+    - JSON response containing credential breach findings and a checksum header.
+    """
     response_data = dmz_sync_methods.dmz_cred_sync(cred_sync_data, current_user)
     # # response_json = json.dumps(response_data, sort_keys=True)
     # Convert response data to a JSON-serializable format
