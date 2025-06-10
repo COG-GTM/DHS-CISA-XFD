94dcaacfdfe56047bc9cfa6abefbee8271ba8a3f
aloftus23 <andylofti@gmail.com>
2025-06-10 14:59:08 -0400

    Update tests to build the new view

--- backend/src/xfd_django/xfd_api/tests/test_domain.py
+++ backend/src/xfd_django/xfd_api/tests/test_domain.py
@@ -10,6 +10,7 @@ import pytest
 from xfd_api.auth import create_jwt_token
 from xfd_api.tasks.helpers.syncdb_helpers.create_db_views import (
     create_domain_materialized_view,
+    create_domain_search_mat_view,
     create_service_mat_view,
     create_vuln_materialized_views,
     create_vuln_normal_views,
@@ -162,6 +163,7 @@ def refresh_vuln_views(django_db_blocker):
             create_domain_materialized_view("mini_data_lake")
             create_vuln_normal_views("mini_data_lake")
             create_vuln_materialized_views("mini_data_lake")
+            create_domain_search_mat_view("mini_data_lake")
 
     return _refresh
 
