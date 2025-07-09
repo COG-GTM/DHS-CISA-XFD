edd874e9423af9b4ab41a600b5d1fa2344cc8000
DJensen94 <79864006+DJensen94@users.noreply.github.com>
2025-07-09 10:06:35 -0600

    move to new line
    move to new line

--- backend/src/xfd_django/xfd_api/helpers/filter_helpers.py
+++ backend/src/xfd_django/xfd_api/helpers/filter_helpers.py
@@ -99,7 +99,8 @@ def apply_domain_filters(domains, filters):
 
 def apply_vuln_filters(
     vulnerabilities: QuerySet, vulnerability_filters: VulnerabilityFilters
-) -> QuerySet:  # pylint: disable=R0912
+) -> QuerySet:
+    # pylint: disable=R0912
     """Filter vulnerabilities using Q objects for partial matches and exact matches."""
     q = Q()
 
