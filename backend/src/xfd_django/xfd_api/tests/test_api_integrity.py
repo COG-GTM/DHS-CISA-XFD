d19e5bfea734df647882102a213f5b73373f6b40
aloftus23 <andylofti@gmail.com>
2025-07-18 12:23:47 -0400

    Revert FAST AM-15. Failing CORS and needs rework

--- backend/src/xfd_django/xfd_api/tests/test_api_integrity.py
+++ backend/src/xfd_django/xfd_api/tests/test_api_integrity.py
@@ -16,7 +16,6 @@ client = TestClient(app)
 PUBLIC_ENDPOINTS = {
     ("POST", "/auth/okta-callback"),
     ("POST", "/auth/callback"),
-    ("POST", "/auth/set-oauth-cookies"),
     ("GET", "/notifications"),
     ("GET", "/healthcheck"),
 }
@@ -58,7 +57,6 @@ EXCLUDED_ENDPOINTS_RESPONSE_MODEL = {
     ("DELETE", "/api-keys/{api_key_id}"),
     ("POST", "/auth/callback"),
     ("POST", "/auth/okta-callback"),
-    ("POST", "/auth/set-oauth-cookies"),
     ("POST", "/domain/export"),
     ("POST", "/vulnerabilities/export"),
     ("DELETE", "/notifications/{notification_id}"),
--- frontend/src/types/pkce-challenge.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'pkce-challenge' {
-  export default function pkceChallenge(length?: number): {
-    code_verifier: string;
-    code_challenge: string;
-  };
-}
