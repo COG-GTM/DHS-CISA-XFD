cfe0dea116ddd47b2ca89282f9183be2bdca0b55
nickviola <nick.viola@associates.cisa.dhs.gov>
2025-08-12 12:47:38 -0500

    Update the default value for first_login field in models.py and legacy user.py model

--- backend/src/xfd_django/xfd_api/schema_models/user.py
+++ backend/src/xfd_django/xfd_api/schema_models/user.py
@@ -209,5 +209,5 @@ class UserResponseV2(BaseModel):
     state: Optional[str] = None
     user_type: Optional[str] = None
     roles: List[Dict[str, Optional[Any]]]
-    first_login: Optional[bool] = None
+    first_login: Optional[bool] = True
     can_select_own_state: Optional[bool] = False
