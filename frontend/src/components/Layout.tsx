7051eb8d26423150bec60ac4ba32ca47f5dff976
Amelia Vance <amelia.vance@associates.cisa.dhs.gov>
2025-08-13 15:38:19 -0400

    Update styling for persistant banner

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -6,7 +6,7 @@ import React, {
 } from 'react';
 import { useLocation } from 'react-router-dom';
 import { withSearch } from '@elastic/react-search-ui';
-import { Alert, AlertTitle, Collapse, Typography } from '@mui/material';
+import { Alert, AlertTitle, Box, Collapse, Typography } from '@mui/material';
 import { styled, useTheme } from '@mui/material/styles';
 import { useMediaQuery } from '@mui/system';
 import { GovBanner, Header } from 'components';
@@ -133,27 +133,45 @@ export const Layout: React.FC<PropsWithChildren<ContextType>> = ({
         </div>
         {!siteWideAlert && user && !noAlertPaths.includes(pathname) && (
           <Collapse in>
-            <Alert severity="info" onClose={handleAlertClose}>
-              <AlertTitle
-                variant="largeBody"
-                color="primary.darker"
-                sx={{ fontWeight: '700' }}
+            <Box sx={{ backgroundColor: '#E5F6FD' }}>
+              <Box
+                display="flex"
+                flexDirection="column"
+                maxWidth="1152px"
+                width="100%"
+                margin="auto"
               >
-                CyHy Dashboard - Beta (Early Access)
-              </AlertTitle>
-              <Typography
-                variant="body1"
-                color="primary.darker"
-                fontWeight="600"
-              >
-                You are using an early release version of the CyHy Dashboard.
-                This site is fully functional, but some features are still being
-                improved and refined. Your feedback during this stage directly
-                shapes improvements. Please go to the Support menu to share
-                feedback, report bugs, or submit questions so we can enhance the
-                dashboard to better meet your needs.
-              </Typography>
-            </Alert>
+                <Alert severity="info" onClose={handleAlertClose}>
+                  <Box
+                    display="flex"
+                    flexDirection="column"
+                    maxWidth="1152px"
+                    width="100%"
+                    margin="auto"
+                  ></Box>
+                  <AlertTitle
+                    variant="largeBody"
+                    color="primary.darker"
+                    sx={{ fontWeight: '700' }}
+                  >
+                    CyHy Dashboard - Beta (Early Access)
+                  </AlertTitle>
+                  <Typography
+                    variant="body1"
+                    color="primary.darker"
+                    fontWeight="600"
+                  >
+                    You are using an early release version of the CyHy
+                    Dashboard. This site is fully functional, but some features
+                    are still being improved and refined. Your feedback during
+                    this stage directly shapes improvements. Please go to the
+                    Support menu to share feedback, report bugs, or submit
+                    questions so we can enhance the dashboard to better meet
+                    your needs.
+                  </Typography>
+                </Alert>
+              </Box>
+            </Box>
           </Collapse>
         )}
         <Header />
