1d8d031f72b29946af7ce16c31036e38e94b3cb2
aloftus23 <andylofti@gmail.com>
2025-08-18 13:13:20 -0400

    update ignore

--- backend/src/xfd_django/xfd_api/tasks/syncdb_task.py
+++ backend/src/xfd_django/xfd_api/tasks/syncdb_task.py
@@ -280,9 +280,9 @@ def update_table(
             """,
                 [table_name],
             )
-            existing_defs = [
+            existing_defs = [  # pylint: disable=unnecessary-comprehension
                 (name, definition) for name, definition in idx_cursor.fetchall()
-            ]  # nosec R1721
+            ]
 
         for model_index in model._meta.indexes:
             if not index_exists_in_db(model_index, existing_defs):
