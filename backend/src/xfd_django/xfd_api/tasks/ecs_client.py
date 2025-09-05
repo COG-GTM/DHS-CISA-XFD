6fbbe6482209f6d5420058205901bab9b12ac366
cduhn17 <87447403+cduhn17@users.noreply.github.com>
2025-09-05 09:48:33 -0500

    Fix environment variable retrieval for QUALYS credentials

--- backend/src/xfd_django/xfd_api/tasks/ecs_client.py
+++ backend/src/xfd_django/xfd_api/tasks/ecs_client.py
@@ -90,9 +90,9 @@ class ECSClient:
                             "WORKER_USER_AGENT": os.getenv("WORKER_USER_AGENT"),
                             "SHODAN_API_KEY": command_options["SHODAN_API_KEY"],
                             "PE_SHODAN_API_KEYS": os.getenv("PE_SHODAN_API_KEYS"),
-                            "QUALYS_USERNAME": os.getenv("QUALYS_USERNAME=dhscs3cd"),
+                            "QUALYS_USERNAME": os.getenv("QUALYS_USERNAME"),
                             "QUALYS_PASSWORD": os.getenv(
-                                "QUALYS_PASSWORD=34Ai*%TYHNM*IKL<drtgh"
+                                "QUALYS_PASSWORD"
                             ),
                             "WHOIS_XML_KEY": os.getenv("WHOIS_XML_KEY"),
                             "WHOIS_XML_THREAD_COUNT": os.getenv(
