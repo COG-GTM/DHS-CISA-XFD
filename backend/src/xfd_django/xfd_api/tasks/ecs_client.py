b3e326e74593d62f071f136d99750fcf751dbd6d
cduhn17 <craig.duhn@nttdatafed.com>
2025-09-08 14:55:53 -0500

    Update ecs_client for lint issues

--- backend/src/xfd_django/xfd_api/tasks/ecs_client.py
+++ backend/src/xfd_django/xfd_api/tasks/ecs_client.py
@@ -91,9 +91,7 @@ class ECSClient:
                             "SHODAN_API_KEY": command_options["SHODAN_API_KEY"],
                             "PE_SHODAN_API_KEYS": os.getenv("PE_SHODAN_API_KEYS"),
                             "QUALYS_USERNAME": os.getenv("QUALYS_USERNAME"),
-                            "QUALYS_PASSWORD": os.getenv(
-                                "QUALYS_PASSWORD"
-                            ),
+                            "QUALYS_PASSWORD": os.getenv("QUALYS_PASSWORD"),
                             "WHOIS_XML_KEY": os.getenv("WHOIS_XML_KEY"),
                             "WHOIS_XML_THREAD_COUNT": os.getenv(
                                 "WHOIS_XML_THREAD_COUNT"
