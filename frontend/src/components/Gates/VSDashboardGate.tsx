7f03db9b78d2eabf52fd79eb74765c5339343470
Amelia Vance <amelia.vance@associates.cisa.dhs.gov>
2025-09-18 17:04:57 -0400

    Update VSDashboardGate.tsx

--- frontend/src/components/Gates/VSDashboardGate.tsx
+++ frontend/src/components/Gates/VSDashboardGate.tsx
@@ -9,6 +9,7 @@ import { useOrgInfo } from 'hooks/useOrgInfo';
 // Shared components
 import NoDataMessage from 'components/Dashboard/NoDataMessage';
 import PageSection from 'components/Dashboard/PageSection';
+import { VulnerabilityScan } from 'pages/VulnerabilityScanDash/VulnerabilityScan';
 // Utils
 import isDataEmpty from 'utils/transformVulnScanData';
 
@@ -23,16 +24,18 @@ export interface WidgetProps extends BoxProps {
   children?: React.ReactNode;
 }
 
+const mailtoLink =
+  'mailto:vulnerability@mail.cisa.dhs.gov?subject=Request%20Assistance%20-%20CyHy%20Dashboard&body=Have%20a%20question%3F%20We%27re%20here%20to%20help.%0A%0AIf%20you%20have%20general%20questions%20about%20the%20CyHy%20Dashboard%2C%20such%20as%20how%20it%20relates%20to%20your%20reports%2C%20how%20to%20view%20data%2C%20or%20how%20to%20connect%20with%20your%20Cybersecurity%20Advisor%20(CSA)%2C%20and%20you%20can%27t%20find%20the%20answer%20in%20the%20Learning%20Center%2C%20you%20can%20send%20your%20question%20to%20us%20here.%0A%0AWe%27re%20excited%20to%20help%20you%20and%20will%20make%20sure%20your%20question%20gets%20to%20the%20right%20place.%0A';
+
 export const VSDashboardGate: React.FC<{
-  contextType: { filters: any; removeFilter: any };
-  children: React.ReactNode;
-}> = (props) => {
-  const { filters, removeFilter } = props.contextType;
+  filters: any;
+  removeFilter: any;
+}> = ({ filters, removeFilter }) => {
   const { currentOrganization, user } = useAuthContext();
 
   useClearFiltersOnMount(filters, removeFilter);
 
-  const { orgId } = useOrgInfo(filters, currentOrganization);
+  const { orgId, orgName } = useOrgInfo(filters, currentOrganization);
 
   const {
     data: vulnScanData,
@@ -70,7 +73,7 @@ export const VSDashboardGate: React.FC<{
             (user?.user_type || 'standard') === 'standard' ? (
               <>
                 Please notify the CyHy team using the{' '}
-                <Link href="mailto:vulnerability@mail.cisa.dhs.gov?subject=Request%20Assistance%20-%20CyHy%20Dashboard&body=Have%20a%20question%3F%20We%27re%20here%20to%20help.%0A%0AIf%20you%20have%20general%20questions%20about%20the%20CyHy%20Dashboard%2C%20such%20as%20how%20it%20relates%20to%20your%20reports%2C%20how%20to%20view%20data%2C%20or%20how%20to%20connect%20with%20your%20Cybersecurity%20Advisor%20(CSA)%2C%20and%20you%20can%27t%20find%20the%20answer%20in%20the%20Learning%20Center%2C%20you%20can%20send%20your%20question%20to%20us%20here.%0A%0AWe%27re%20excited%20to%20help%20you%20and%20will%20make%20sure%20your%20question%20gets%20to%20the%20right%20place.%0A">
+                <Link href={mailtoLink} target="_blank" rel="noopener">
                   General Questions
                 </Link>{' '}
                 option in the Support menu.
@@ -107,7 +110,11 @@ export const VSDashboardGate: React.FC<{
     );
   }
   return (
-    <VulnerabilityScanWithSearch orgId={orgId} vulnScanData={vulnScanData} />
+    <VulnerabilityScan
+      orgId={orgId}
+      orgName={orgName}
+      vulnScanData={vulnScanData}
+    />
   );
 };
 
