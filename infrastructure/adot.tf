ed3e85135e9f9c375fe300aa3c00cc65c5068c01
cduhn17 <87447403+cduhn17@users.noreply.github.com>
2025-09-30 07:57:34 -0500

    Update infrastructure/adot.tf at vpc_id to better align with other variable definition
    Co-authored-by: aloftus23 <79927030+aloftus23@users.noreply.github.com>

--- infrastructure/adot.tf
+++ infrastructure/adot.tf
@@ -77,7 +77,7 @@ resource "aws_vpc_endpoint" "xray" {
 # CloudWatch Logs endpoint service name: com.amazonaws.${region}.logs
 resource "aws_vpc_endpoint" "logs" {
   count               = (local.vpc_id != null && length(local.subnets_ep) > 0) ? 1 : 0
-  vpc_id              = local.vpc_id
+  vpc_id              = var.is_dmz ? aws_vpc.crossfeed_vpc[0].id : data.aws_ssm_parameter.vpc_id[0].value
   service_name        = "com.amazonaws.${var.aws_region}.logs"
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
